46b8931c8973c50f87072c735505533fd7cdb4a3
Geoffrey-Anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2022-09-07 22:45:39 +0530

    Added Agg Form

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import RegisterForm from './Pages/RegisterForm'
-import BankRegister from './Pages/BankRegister'
-import Login from './Pages/Login'
-import BankLogin from './Pages/BankLogin'
-import MobileOTP from './Pages/MobileOTP'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import logo from "./logo.svg";
+import "./App.css";
+import RegisterForm from "./Pages/RegisterForm";
+import BankRegister from "./Pages/BankRegister";
+import Login from "./Pages/Login";
+import BankLogin from "./Pages/BankLogin";
+import MobileOTP from "./Pages/MobileOTP";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AggForm from "./Pages/AggForm";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/userRegister" element={<RegisterForm/>}/>
-      <Route path="/bankRegister" element={<BankRegister/>}/>
-      <Route path="/bankLogin" element={<BankLogin/>}/>
-      <Route path="/userlogin" element={<Login/>}/>
-      <Route path="/otp" element={<MobileOTP/>}></Route>
-      <Route path="/aggForm" element={<>Form</>}></Route>
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/userRegister" element={<RegisterForm />} />
+        <Route path="/bankRegister" element={<BankRegister />} />
+        <Route path="/bankLogin" element={<BankLogin />} />
+        <Route path="/userlogin" element={<Login />} />
+        <Route path="/otp" element={<MobileOTP />}></Route>
+        <Route path="/aggForm" element={<AggForm />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/AggForm.js
+++ src/Pages/AggForm.js
@@ -1,43 +1,88 @@
-import React from 'react'
+import React from "react";
 
 const AggForm = () => {
-  return (
-    <div class="form-style-5">
-<form>
-<fieldset>
-<legend><span class="number">1</span> Candidate Info</legend>
-<input type="text" name="field1" placeholder="Your Name *">
-<input type="email" name="field2" placeholder="Your Email *">
-<textarea name="field3" placeholder="About yourself"></textarea>
-<label for="job">Interests:</label>
-<select id="job" name="field4">
-<optgroup label="Indoors">
-  <option value="fishkeeping">Fishkeeping</option>
-  <option value="reading">Reading</option>
-  <option value="boxing">Boxing</option>
-  <option value="debate">Debate</option>
-  <option value="gaming">Gaming</option>
-  <option value="snooker">Snooker</option>
-  <option value="other_indoor">Other</option>
-</optgroup>
-<optgroup label="Outdoors">
-  <option value="football">Football</option>
-  <option value="swimming">Swimming</option>
-  <option value="fishing">Fishing</option>
-  <option value="climbing">Climbing</option>
-  <option value="cycling">Cycling</option>
-  <option value="other_outdoor">Other</option>
-</optgroup>
-</select>      
-</fieldset>
-<fieldset>
-<legend><span class="number">2</span> Additional Info</legend>
-<textarea name="field3" placeholder="About Your School"></textarea>
-</fieldset>
-<input type="submit" value="Apply" />
-</form>
-</div>
-  )
-}
+  const [expenses, setExpenses] = React.useState("");
+  const [savings, setSavings] = React.useState("");
+  const [taxUrl, setTaxUrl] = React.useState("");
+  const [portfolioUrl, setPortfolioUrl] = React.useState("");
 
-export default AggForm
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(expenses, savings, taxUrl, portfolioUrl);
+    fetch(`http://localhost:3001/user/addDetails`, {
+      method: "PUT",
+      body: JSON.stringify({
+        expenses,
+        savings,
+        taxUrl,
+        portfolioUrl,
+        bankName: localStorage.getItem("user_ifsc") || "SBI",
+        mobileNo: JSON.parse(localStorage.getItem("user")).mobileNo,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": true,
+      },
+    })
+      .then((rawResponse) => {
+        return rawResponse.json();
+      })
+      .then((response) => {
+        window.location.href = "/dashboard";
+      });
+    setExpenses("");
+    setSavings("");
+    setTaxUrl("");
+    setPortfolioUrl("");
+  };
+
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-blue-100 flex-col gap-y-9">
+      <p className="text-3xl font-semibold text-center">
+        Add Financial Details
+      </p>
+      <form
+        onSubmit={submitHandler}
+        className="h-[70%] w-[80%] md:w-[60%] border-2 border-black bg-white flex flex-col items-center justify-around rounded-md"
+      >
+        <input
+          type="text"
+          value={expenses}
+          onChange={(e) => setExpenses(e.target.value)}
+          className="w-[50%] h-12 border-2 border-black p-2 rounded-md"
+          placeholder="Expenses"
+        />
+        <input
+          type="text"
+          value={savings}
+          onChange={(e) => setSavings(e.target.value)}
+          className="w-[50%] h-12 border-2 border-black p-2 rounded-md"
+          placeholder="Savings"
+        />
+        <input
+          type="text"
+          value={taxUrl}
+          onChange={(e) => setTaxUrl(e.target.value)}
+          className="w-[50%] h-12 border-2 border-black p-2 rounded-md"
+          placeholder="Tax Url"
+        />
+        <input
+          type="text"
+          value={portfolioUrl}
+          onChange={(e) => setPortfolioUrl(e.target.value)}
+          className="w-[50%] h-12 border-2 border-black p-2 rounded-md"
+          placeholder="Portfolio Url"
+        />
+        <button
+          type="submit"
+          className="px-10 py-2 rounded-md border-2 border-black hover:bg-black hover:text-white hover:border-white hover:border-2"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AggForm;
